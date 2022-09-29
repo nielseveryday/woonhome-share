3f33c19efd2e670ca93aa0548d52f34e95ad147d
Niels Havermans <nielshavermans@ibizz.nl>
2022-09-29 11:04:48 +0200

    logging

--- src/index.php
+++ src/index.php
@@ -3,16 +3,11 @@
 // Autoload files using the Composer autoloader.
 require_once __DIR__ . '/vendor/autoload.php';
 
-
-
 use WoonhomeShare\WoonhomeShare;
 use Structure\Structure;
 
 // Init structure
 Structure::init();
-
-var_dump('share');
-exit;
 
 $share = new WoonhomeShare();
 echo $share->start();
